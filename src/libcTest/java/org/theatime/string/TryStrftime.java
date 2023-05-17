5bc930ac854963d17831a6da332ac8115adc0212
Dai MIKURUBE <dmikurube@acm.org>
2023-05-17 20:58:51 +0900

    [WIP]

---
+++ src/libcTest/java/org/theatime/string/TryStrftime.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2023 Dai MIKURUBE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.theatime.string;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class TryStrftime {
+    /*
+    CRubyCaller(final String rubyPath) {
+        this.rubyPath = rubyPath;
+    }
+
+    CRubyCaller() {
+        this("ruby");
+    }
+
+    String getSingleLineFromOneLiner(final String oneLine) throws IOException, InterruptedException {
+        final List<String> result = this.callOneLiner(oneLine);
+        assertEquals(1, result.size());
+        return result.get(0);
+    }
+
+    List<String> callOneLiner(final String oneLine) throws IOException, InterruptedException {
+        final String oneLineEscaped = oneLine.replaceAll("\'", "\\\'");
+        final ProcessBuilder processBuilder = new ProcessBuilder(this.rubyPath, "-e", oneLineEscaped);
+        final Map<String, String> environment = processBuilder.environment();
+        environment.put("TZ", "UTC");
+
+        final Process process = processBuilder.start();  // IOException possibly.
+        final int exitStatus = process.waitFor();  // InterruptedException possibly.
+
+        final byte[] buffer = new byte[8192];
+
+        final ByteArrayOutputStream stderr = new ByteArrayOutputStream();
+        final InputStream stderrInputStream = process.getErrorStream();
+        while (true) {
+            final int lengthRead = stderrInputStream.read(buffer);
+            if (0 > lengthRead) {
+                break;
+            }
+            stderr.write(buffer, 0, lengthRead);
+        }
+
+        if (stderr.size() > 0) {
+            final ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+            final InputStream stdoutInputStream = process.getInputStream();
+            while (true) {
+                final int lengthRead = stdoutInputStream.read(buffer);
+                if (0 > lengthRead) {
+                    break;
+                }
+                stdout.write(buffer, 0, lengthRead);
+            }
+            throw new RuntimeException(
+                    "Detected output in standard error.\n" +
+                    "stdout: " + stdout.toString() + "\n" +
+                    "stderr: " + stderr.toString());
+        }
+
+        final InputStream stdout = process.getInputStream();
+        final List<String> lines;
+        try (final BufferedReader reader = new BufferedReader(new InputStreamReader(stdout))) {
+            lines = reader.lines().collect(Collectors.toList());
+        }
+        return lines;
+    }
+
+    private final String rubyPath;
+    */
+}
